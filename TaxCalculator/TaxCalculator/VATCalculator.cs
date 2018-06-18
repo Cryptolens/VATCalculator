d127279d8f51895f729d99a2896e39f8e9885b0a
Artem Los <artem@cryptolens.io>
2018-06-18 16:48:59 +0200

    First commit

---
+++ TaxCalculator/TaxCalculator/VATCalculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using VATCalculator.Properties;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Cryptolens.VATCalculator
+{
+    public class VATCalculator
+    {
+        public static int CalculateVAT(string ISOCountryName, string VATId)
+        {
+            JsonConvert.DeserializeObject<Dictionary<string,string>>(Resources.ISO2Country);
+
+            return 0;
+        }
+    }
+}
---
+++ TaxCalculator/XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+using Cryptolens.VATCalculator;
+
+namespace XUnitTestProject1
+{
+    public class TaxCalculatorTest
+    {
+        [Fact]
+        public void TestCalculateTax()
+        {
+            //TaxCalculator.CalculateTax();
+        }
+    }
+}
